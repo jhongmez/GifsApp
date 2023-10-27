0d6860b0033a9290fb9b5d2e6482aee41fde1681
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-10-27 07:02:22 -0500

    Add viewchild for get text input

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'gifs-search-box',
-  template: `
-    <h5>Buscar:</h5>
-    <input 
-    type="text" 
-    class="form-control" 
-    placeholder="buscar gifs..."
-    />
-  `,
+	selector: 'gifs-search-box',
+	template: `
+		<h5>Buscar:</h5>
+		<input 
+		type="text" 
+		class="form-control" 
+		placeholder="buscar gifs..."
+		(keyup.enter)="searchTag()"
+		#txtTagInput
+		/>
+	`,
 })
 export class SearchBoxComponent {
+	
+	// * VIEWCHILD: Nos sirve para tomar una referencia local
+	// * VIEWCHILDREN: Toma todos los input y regresa un arreglo de los elementos HTML
+	
+	@ViewChild('txtTagInput')
+	tagInput!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+	constructor() { }
+	
+	// * Se utilizo el #txtTagInput como referencia para no crear un formsmodule o ngmodels para este manejo
+	searchTag() {
+		const newTag = this.tagInput.nativeElement.value;
+		console.log({ newTag });
+	}
 
 }
