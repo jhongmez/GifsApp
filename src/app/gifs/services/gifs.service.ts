109a1509a8a4b77c4bdc9504f01d3fbd62e52d87
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-12 17:12:03 -0500

    Update get service

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
@@ -5,8 +6,10 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 	
 	private _tagsHistory: string[] = [];
+	private apiKey: string = 'AtmeiulbDDuSWWTGn0DNEdtEI0JiErCB';
+	private serviceUrl: string = 'https://api.giphy.com/v1/gifs/';
 
-	constructor() { }
+	constructor( private http: HttpClient) { }
 
 	get tagsHistory() {
 		return [...this._tagsHistory];
@@ -26,8 +29,17 @@ export class GifsService {
 	public searchTag( tag: string ): void {
 
 		if ( tag.length === 0 ) return;
-
 		this.organizeHistory( tag );
+
+		const params = new HttpParams()
+			.set('api_key', this.apiKey)
+			.set('limit', 10)
+			.set('q', tag)
+
+		this.http.get(`${ this.serviceUrl }search`, { params })
+			.subscribe( resp => {
+				console.log(resp);
+			})
 	}
 
 }
