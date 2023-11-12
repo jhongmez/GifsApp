36930d3662a1814298eee99b34fc1ead33dba5f2
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-12 13:17:57 -0500

    Add validation for tags history

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,10 +11,23 @@ export class GifsService {
 	get tagsHistory() {
 		return [...this._tagsHistory];
 	}
+
+	private organizeHistory( tag: string ) {
+		tag = tag.toLowerCase();
+
+		if( this._tagsHistory.includes(tag)) {
+			this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag )
+		}
+
+		this._tagsHistory.unshift( tag );
+		this._tagsHistory = this._tagsHistory.slice( 0,10 );
+	}
 	
 	public searchTag( tag: string ): void {
-		this._tagsHistory.unshift( tag );
-		console.log(this.tagsHistory);
+
+		if ( tag.length === 0 ) return;
+
+		this.organizeHistory( tag );
 	}
 
 }
