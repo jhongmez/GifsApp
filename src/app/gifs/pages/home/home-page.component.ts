637702dc82e09f1f06128d228779ef3de9e32775
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-13 08:35:16 -0500

    Add data into card list

--- src/app/gifs/pages/home/home-page.component.ts
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
-  selector: 'gifs-home-page',
-  templateUrl: './home-page.component.html',
+	selector: 'gifs-home-page',
+	templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
 
-  constructor() { }
+	constructor( private gifsService: GifsService ) { }
+
+	get gifs(): Gif[] {
+		return this.gifsService.gifList;
+	}
 
 }
